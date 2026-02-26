40695eb62d5e14ae9feec61e6af4c0d37433167d
Duane King <duane@moonshot.ooo>
2026-02-25 21:50:57 -0800

    Bump SW cache to v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oblique-v5';
+const CACHE = 'oblique-v6';
 const ASSETS = ['/', '/index.html', '/manifest.json',
                 '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png'];
 
